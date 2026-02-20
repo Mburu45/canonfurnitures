ce15ab1b01e9385d048d08c11b3bcc3c10c776ad
Mburu45 <mburulawrence6@gmail.com>
2026-02-20 10:30:27 +0300

    Fix homepage route to use home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
@@ -23,7 +22,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
 Route::redirect('/cart', '/shop')->name('cart.index');
